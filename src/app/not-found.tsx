ac271a67fb671c8f3df4c10f94dc755668e7364e
kors-ak <alinakorsak86@gmail.com>
2026-07-18 15:05:31 +0300

    feat: добавление страницы 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,36 @@
+import Bar from '@/components/Bar/Bar';
+import Image from 'next/image';
 import Link from 'next/link';
+import style from './not-found.module.css';
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>Страница не найдена</h1>
-      <Link href="/">На главную</Link>
+    <div className={style.wrapper}>
+      <div className={style.container}>
+        <div className={style.card}>
+          <div className={style.code}>404</div>
+          <div className={style.title}>
+            Страница не найдена{' '}
+            <Image
+              src="/img/emoji_crying.png"
+              alt="crying emoji"
+              width={52}
+              height={52}
+            />
+          </div>
+          <div className={style.subtitle}>
+            Возможно, она была удалена
+            <br />
+            или перенесена на другой адрес
+          </div>
+
+          <Link href="/" className={style.button}>
+            Вернуться на главную
+          </Link>
+        </div>
+
+        <Bar />
+      </div>
     </div>
   );
 }
