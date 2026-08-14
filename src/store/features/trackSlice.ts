6877a3508fc923dbed9024c9f78496122ee20227
kors-ak <alinakorsak86@gmail.com>
2026-08-14 23:23:25 +0300

    fix: добавляет проверку для завершения воспроизведения на последнем треке

--- src/store/features/trackSlice.ts
+++ src/store/features/trackSlice.ts
@@ -54,6 +54,8 @@ const trackSlice = createSlice({
           (el) => el._id === state.currentTrack?._id
         )
 
+        if (currentTrackIndex === currentPlaylist.length - 1) return // для соответствия чек-листу. Уточнить, можно ли начинать плейлист сначала
+
         const nextTrackIndex =
           currentTrackIndex === currentPlaylist.length - 1
             ? 0
